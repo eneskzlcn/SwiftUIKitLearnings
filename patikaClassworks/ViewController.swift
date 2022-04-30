96051d5e1b4f106c2a9550a203d11537651d75cf
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-04-30 07:36:12 +0300

    fast commit

--- patikaClassworks/ViewController.swift
+++ patikaClassworks/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let username = "eneskzlcn"
+    let password = "enes"
+    
+    @IBOutlet weak var usernameTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var infoLBL: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func onTapWelcomeButton(_ sender: UIButton) {
+        if let usernameInp = usernameTF.text, let passwordInp = passwordTF.text{
+            
+            if usernameInp == username, passwordInp == password {
+                infoLBL.textColor = .green
+                infoLBL.text = "You logged in."
+            }
+            else {
+                infoLBL.textColor = .red
+                infoLBL.text = "Wrong username or password"
+            }
+        }else {
+            infoLBL.textColor = .brown
+            infoLBL.text = "Fill the blanks"
+        }
+    }
+    
 }
 
